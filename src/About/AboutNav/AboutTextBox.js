45a56a5d95576443e5c3d38fe5cecc4a07844f92
Dean-Baylem <dbaylem5822@gmail.com>
2023-03-10 08:45:11 +0900

    3 / 10 / 2023
    Work started on making the pages dynamic and changing details based on screen sizes.

--- src/About/AboutNav/AboutTextBox.js
+++ src/About/AboutNav/AboutTextBox.js
@@ -10,11 +10,12 @@ const AboutTextBox = props => {
         <div className="about-subtitle">
           <h5>{props.subtitle}</h5>
         </div>
-        <div>
+        <div className="about-image-box">
           {props.body === "image" ? (
-            <img 
+            <img
               src="https://img.freepik.com/free-vector/people-ask-question_102902-2341.jpg?w=740&t=st=1678067967~exp=1678068567~hmac=dae68e2813a69b66998b329040bd77032bc8b172532a2ef7f0a0fec30347546a"
               alt="about-me"
+              className="about-image"
             />
           ) : (
             props.body.split("\n").map((text) => <p>{text}</p>)
